489d121674c55d3eb91c5c61f28f10645336009a
Simran <simran5590@gmail.com>
2022-05-30 20:25:54 -0700

    Added NetworkingClient and Data extension with decoding strategy

--- Extensions/Foundation+Extensions/Data+Extensions.swift
+++ Extensions/Foundation+Extensions/Data+Extensions.swift
@@ -7,4 +7,20 @@ extension Data {
     func toObject<T : Decodable>(ofType type: T.Type) throws -> T {
         return  try JSONDecoder().decode(T.self, from: self)
     }
+
+    func toObject<T : Decodable>(toType type: T.Type,
+                                     dateDecodingStrategy: JSONDecoder.DateDecodingStrategy? = nil,
+                                     keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy? = nil) throws -> T {
+        let decoder = JSONDecoder()
+        
+        if let dateDecodingStrategy = dateDecodingStrategy {
+            decoder.dateDecodingStrategy = dateDecodingStrategy
+        }
+        
+        if let keyDecodingStrategy = keyDecodingStrategy {
+            decoder.keyDecodingStrategy = keyDecodingStrategy
+        }
+        
+        return  try decoder.decode(T.self, from: self)
+    }
 }
